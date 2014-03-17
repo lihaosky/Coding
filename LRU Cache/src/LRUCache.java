e3fe2d03abbe65a3861b453545fdd6018c02c3ae
unknown <hali3@hali3-WS.cisco.com>
2014-03-17 12:29:22 -0700

    LRU cache...

--- LRU Cache/src/LRUCache.java
+++ LRU Cache/src/LRUCache.java
@@ -10,9 +10,111 @@ set(key, value) - Set or insert the value if the key is not already present. Whe
  *
  */
 
+class Node {
+	int key;
+	int val;
+	Node prev;
+	Node next;
+	
+	public Node(int key, int val) {
+		this.key = key;
+		this.val = val;
+		this.prev = null;
+		this.next = null;
+	}
+}
+
+class LRUList {
+	Node head, tail;
+	int size;
+	
+	public LRUList() {
+		head = tail = null;
+		size = 0;
+	}
+	
+	public Node remove(Node node) {
+		if (size == 0) {
+			return null;
+		}
+		if (size == 1) {
+			size--;
+			head = tail = null;
+			return node;
+		}
+		if (node == head) {
+			node.next.prev = node.prev;
+			head = node.next;
+			node.next = node.prev = null;
+			size--;
+			return node;
+		}
+		if (node == tail) {
+			node.prev.next = node.next;
+			tail = node.prev;
+			node.prev = node.next = null;
+			size--;
+			return node;
+		}
+		node.next.prev = node.prev;
+		node.prev.next = node.next;
+		node.next = node.prev = null;
+		size--;
+		return node;
+	}
+	
+	public Node removeTail() {
+		if (size == 0) {
+			return null;
+		}
+		if (head == tail) {
+			Node tmp = head;
+			head = tail = null;
+			size--;
+			return tmp;
+		}
+		Node tmp = tail;
+		tail.prev.next = null;
+		tail = tail.prev;
+		tmp.next = tmp.prev = null;
+		size--;
+		return tmp;
+	}
+	
+	public void add(Node node) {
+		if (head == null && tail == null) {
+			head = tail = node;
+			size++;
+			return;
+		}
+		node.next = head;
+		head.prev = node;
+		head = node;
+		size++;
+	}
+	
+	public void print() {
+		Node node = head;
+		while (node != null) {
+			System.out.println(node.key + " " + node.val);
+			node = node.next;
+		}
+		if (head != null) {
+			System.out.println("head is " + head.key);
+		} else {
+			System.out.println("head is null");
+		}
+		if (tail != null) {
+			System.out.println("Tail is " + tail.key);
+		} else {
+			System.out.println("Tail is null");
+		}
+	}
+}
+
 public class LRUCache {
-	private LinkedList<Integer> lruList = new LinkedList<Integer>();
-	private Hashtable<Integer, Integer> keyValueMap = new Hashtable<Integer, Integer>();
+	public LRUList list = new LRUList();
+	private Hashtable<Integer, Node> keyValueMap = new Hashtable<Integer, Node>();
 	private int capacity;
 	private int currentSize;
 	
@@ -22,35 +124,50 @@ public class LRUCache {
     }
     
     public int get(int key) {
-        Integer value = keyValueMap.get(key);
+        Node value = keyValueMap.get(key);
         if (value == null) {
         	return -1;
         }
-        lruList.remove(new Integer(key));
-        lruList.add(0, new Integer(key));
-        return value;
+        list.remove(value);
+        list.add(value);
+        return value.val;
     }
     
     public void set(int key, int value) {
-        Integer retVal = keyValueMap.get(key);
+        Node retVal = keyValueMap.get(key);
     	Integer keyObj = new Integer(key);
-    	Integer valueObj = new Integer(value);
+    	Node valueObj = new Node(key, value);
         if (retVal != null) {
         	keyValueMap.put(keyObj, valueObj);
-        	lruList.remove(retVal);
-        	lruList.add(0, keyObj);
+        	list.remove(retVal);
+        	list.add(valueObj);
         } else {
         	if (currentSize == capacity) {
-        		Integer lastKey = lruList.getLast();
-        		lruList.removeLast();
-        		keyValueMap.remove(lastKey);
-        		keyValueMap.put(keyObj, valueObj);
-        		lruList.add(0, keyObj);
+        		Node target = list.removeTail();
+        		keyValueMap.remove(target.key);
+        		Node node = new Node(key, value);
+        		keyValueMap.put(key, node);
+        		list.add(node);
         	} else {
-        		keyValueMap.put(key, value);
-        		lruList.add(0, new Integer(key));
+        		Node node = new Node(key, value);
+        		keyValueMap.put(key, node);
+        		list.add(node);
         		currentSize++;
         	}
         }
+    }
+    
+    public static void main(String[] args) {
+    	LRUCache lc = new LRUCache(2);
+    	lc.set(2, 1);
+    	lc.list.print();
+    	lc.set(2, 2);
+    	lc.list.print();
+    	System.out.println(lc.get(2));
+    	lc.set(1, 1);
+    	lc.list.print();
+    	lc.set(4,  1);
+    	lc.list.print();
+    	System.out.println(lc.get(2));
     }
 }
